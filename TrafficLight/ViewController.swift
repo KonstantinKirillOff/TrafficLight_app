9a60a0d46201f46b46ad04b3b698d0049856c6a7
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2021-08-18 18:35:24 +0300

    add logic for interface elements

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var signalsOfTrafickLite: [UIView]!
+    @IBOutlet weak var startButton: UIButton!
+    var currentSignal = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        for signal in signalsOfTrafickLite {
+            signal.alpha = 0.3
+            signal.layer.cornerRadius = 60
+        }
     }
 
-
+    @IBAction func startButtonPressed() {
+        startButton.setTitle("Next", for: .normal)
+        
+        for signal in 0...2 {
+            
+            if signal == currentSignal {
+                signalsOfTrafickLite[signal].alpha = 1
+            } else {
+                signalsOfTrafickLite[signal].alpha = 0.3
+            }
+        }
+        
+        if currentSignal == 2 {
+            currentSignal = 0
+        } else {
+            currentSignal += 1
+        }
+        
+    }
+    
 }
 
